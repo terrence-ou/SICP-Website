b2f24c391e6549e062d3fffde6d83c40631354b9
terrence-ou <outerrencedl@gmail.com>
2024-03-17 23:55:22 -0400

    base layout updated

---
+++ src/site.config.ts
@@ -0,0 +1,14 @@
+export const menuLinks: Array<{ title: string; path: string }> = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Info",
+    path: "/info/",
+  },
+  {
+    title: "Solutions",
+    path: "/posts/",
+  },
+];
