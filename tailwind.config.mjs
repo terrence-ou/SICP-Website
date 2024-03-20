8be33eaf792128d9422a3e0eb6e05a198809feed
terrence-ou <outerrencedl@gmail.com>
2024-03-20 15:54:57 -0400

    page setup updated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [...fontFamily.sans],
+        serif: [...fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
+};
